346c9fb35d1811392e287f2b012223e9c5b1063e
ManuCiao <mn.sabatino@gmail.com>
2016-11-29 14:39:04 +0000

    create a new transaction and tes

--- lib/transaction_history.rb
+++ lib/transaction_history.rb
@@ -1,9 +1,15 @@
+require_relative 'transaction'
+
 class Transaction_history
 
-  attr_reader :history
+  attr_reader :history, :transaction
 
   def initialize
     @history = [];
+  end
+
+  def create_history(credit, deposit, new_balance)
+    @transaction = Transaction.new(credit, deposit, new_balance)
   end
 
 end
--- spec/transaction_history_spec.rb
+++ spec/transaction_history_spec.rb
@@ -9,4 +9,11 @@ describe Transaction_history do
       end
   end
 
+  context 'returns an a transaction' do
+      it 'should return a transaction in the history' do
+        transaction_history.create_history(300,200,500)
+        expect(transaction_history.transaction).to be_an_instance_of(Transaction)
+      end
+  end
+
 end
